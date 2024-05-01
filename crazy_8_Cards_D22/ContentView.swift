c35e5092b90247518e47153e5e29f16b0a42ef12
Mabende <belindamabende@gmail.com>
2024-05-01 20:15:50 +0200

    Initial Commit

---
+++ crazy_8_Cards_D22/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  crazy_8_Cards_D22
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ crazy_8_Cards_D22/crazy_8_Cards_D22App.swift
@@ -0,0 +1,17 @@
+//
+//  crazy_8_Cards_D22App.swift
+//  crazy_8_Cards_D22
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/05/01.
+//
+
+import SwiftUI
+
+@main
+struct crazy_8_Cards_D22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
